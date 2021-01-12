60d9f23c44e2428167b974d3bc6daee3ae37e8eb
liuyixuan <lyx182581@163.com>
2021-01-12 14:32:09 +0800

    Update 01/12

--- test.sol
+++ test.sol
@@ -1,18 +1,18 @@
-solidity pragma >0.4.7 <= 0.9.0;
+pragma solidity >0.4.7 <= 0.9.0;
 
 contract Opt {
   uint x ;
 
-  function cheap () external pure returns (uint) {
+  function cheap () external returns (uint) {
     uint y = x;
     for(uint i = 0; i < 3; i++ ){
       y++;
     }
-    x = y
-    return x;
+
+    return y;
   }
 
-  function expe () external pure returns (uint) {
+  function expe () external returns (uint) {
 
     for(uint i = 0; i < 3; i++ ){
       x++ ;
@@ -22,14 +22,3 @@ contract Opt {
 }
 
 
-
-// 不要使用这个合约，其中包含一个 bug。
-contract Fund {
-    /// 合约中 |ether| 分成的映射。
-    mapping(address => uint) shares;
-    /// 提取你的分成。
-    function withdraw() public {
-        if (msg.sender.send(shares[msg.sender]))
-            shares[msg.sender] = 0;
-    }
-}
